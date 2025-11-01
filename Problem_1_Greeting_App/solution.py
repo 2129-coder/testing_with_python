e70394a080d1d2fb8ce3049d0846ae9c0d3bfff4
Estifanos Abebaw <108541123+esti-tech@users.noreply.github.com>
2025-11-01 23:59:59 +0300

    Implement f-string for greeting output
    Added f-string formatting to the greeting message.

--- Problem_1_Greeting_App/solution.py
+++ Problem_1_Greeting_App/solution.py
@@ -3,4 +3,5 @@ name=input("Enter your full name: ")
 favorite_food=input("Enter your favorite food: ")
 age=int(input("Enter your age: "))
 birth_year=current_year-age
+#you need to add aan f string to prin a name here
 print ("Hello{name}.Your favorite food is{favorite_food}.Your birth year is{birth_year}.")
